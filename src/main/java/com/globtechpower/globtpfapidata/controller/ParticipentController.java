58938c7ee49a98804b23eec742139933de88bf4e
KunjanThakor <kunjan7634@gmail.com>
2024-05-24 09:58:11 +0530

    Remove security temporary for Error:"CrossOrigin"

--- src/main/java/com/globtechpower/globtpfapidata/controller/ParticipentController.java
+++ src/main/java/com/globtechpower/globtpfapidata/controller/ParticipentController.java
@@ -2,6 +2,7 @@ package com.globtechpower.globtpfapidata.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,18 +21,20 @@ import com.globtechpower.globtpfapidata.repository.ParticipentRepository;
 import com.globtechpower.globtpfapidata.service.ParticipentService;
 
 @RestController
-//@CrossOrigin(origins = "*")
-@CrossOrigin
+@CrossOrigin(origins = "*")
+//@CrossOrigin
 public class ParticipentController {
 
-	 @Autowired
-	  private PasswordEncoder passwordEncoder;  // Inject PasswordEncoder bean
+	@Autowired
+	private PasswordEncoder passwordEncoder; // Inject PasswordEncoder bean
 	@Autowired
 	ParticipentService participentService;
-	
+	@Autowired
+	UserServiceImp userServiceImp;
+
 	@Autowired
 	ParticipentRepository pr;
-	
+
 	@PostMapping("/addparticpent")
 	@ResponseBody
 	public Participent addData(@RequestBody Participent particpent) {
@@ -40,7 +44,7 @@ public class ParticipentController {
 		particpent.setRoles(nr);
 		return participentService.addParticipent(particpent);
 	}
-	
+
 	@PostMapping("/adddt")
 	public Participent addDatatwo(@RequestBody Participent particpent) {
 		particpent.setPassword(passwordEncoder.encode(particpent.getPassword()));
@@ -49,27 +53,30 @@ public class ParticipentController {
 		particpent.setRoles(nr);
 		return participentService.addParticipent(particpent);
 	}
-	
+
 	@GetMapping("/sec/showpart")
 	public List<Participent> showData() {
 		return participentService.getParticipents();
 	}
+
 	@GetMapping("/auth")
 	public void showPart() {
-		new UserServiceImp();
-	}
 	
+	}
+
 	@GetMapping("/sec/fnb/{email}")
 	public Participent showDataTwo(@PathVariable String email) {
 		return pr.findByEmail(email);
 	}
-	
-	@GetMapping("/bylogin/{email}/{passowrd}")
-	public Participent getLogin(@PathVariable String email, @PathVariable String passowrd) {
+
+	@PostMapping("/bylogin")
+	public Participent getLogin(@RequestBody Map<String, String> loginData) {
+		String email = loginData.get("email");
+		String password = loginData.get("password");
 		Participent p = participentService.getByEmial(email);
-		boolean isPasswordMatch = passwordEncoder.matches(passowrd,p.getPassword() );
-		if (isPasswordMatch) {	
-			return participentService.getByEmialAndPassowrd(email, p.getPassword());
+		if (p != null && passwordEncoder.matches(password, p.getPassword())) {
+//			userServiceImp.loadUserByUsername(email);
+			return p;
 		} else {
 			return null;
 		}
